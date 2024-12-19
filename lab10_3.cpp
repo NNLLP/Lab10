d185daf935c74e0894aafbf099421039ea9b9031
NNLLP <98796613+NNLLP@users.noreply.github.com>
2024-12-19 21:31:38 +0700

    lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    ifstream source("score.txt");
+    vector<double> scores;
+    double num, sum = 0, mean, stddev = 0;
+    while (source >> num) {
+        scores.push_back(num);
+        sum += num;
+    }
+    source.close();
+    int n = scores.size();
+    if (n == 0) {
+        cout << "No data available in the file." << endl;
+        return 0;
+    }
+    mean = sum / n;
+    for (double score : scores) {
+        stddev += pow(score - mean, 2);
+    }
+    stddev = sqrt(stddev / n);
+    cout << "Number of data = " << n << endl;
+    cout << "Mean = " << setprecision(3) << mean << endl;
+    cout << "Standard deviation = " << setprecision(3) << stddev << endl;
+    return 0;
+}
